0f6151e17b9b63122c179543333ed19219b34bc1
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-08-13 22:29:52 +0530

    Button Links

--- Assets/Prefabs/Background/BackgroundCrtl.cs
+++ Assets/Prefabs/Background/BackgroundCrtl.cs
@@ -8,9 +8,32 @@ public class BackgroundCrtl : MonoBehaviour
     [SerializeField] private Image background;
     [SerializeField] private Image foreground;
     [SerializeField] private PlaybackScreen screen;
+    [Space]
+    [SerializeField] private List<Sprite> bgSprites;
+
+    private int currentBGIndex = 0;
 
     public void OnImageReady(IMGProcesser imgP)
     {
         screen.SetScreenTexture(imgP.maskedCamFeed);
+    }
+
+    public void ChangeBG(int deltaIndex)
+    {
+        currentBGIndex += deltaIndex;
+
+        if(currentBGIndex > bgSprites.Count - 1)
+            currentBGIndex = 0;
+        else if(currentBGIndex < 0)
+            currentBGIndex = bgSprites.Count - 1;
+
+        Debug.Log(currentBGIndex);
+
+        background.sprite = bgSprites[currentBGIndex];
+    }
+
+    public void ToggleDesk(bool state)
+    {
+        foreground.enabled = state;
     }
 }
---
+++ Assets/Prefabs/Controls/UIControllers.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControllers : MonoBehaviour
+{
+
+    public void ChangeBG()
+    {
+
+    }
+}
