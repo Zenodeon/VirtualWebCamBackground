85b86ca3375f4f777c4a7fcaa84fe3155e12d84f
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-08-13 22:56:11 +0530

    GroupFade

--- Assets/Prefabs/Controls/UIControllers.cs
+++ Assets/Prefabs/Controls/UIControllers.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using DG.Tweening;
+
 public class UIControllers : MonoBehaviour
 {
+    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [Space]
+    [SerializeField] private float duration = 0.3f;
 
-    public void ChangeBG()
+    public void Capture()
     {
-
+        canvasGroup.DOFade(0, duration);
     }
 }
